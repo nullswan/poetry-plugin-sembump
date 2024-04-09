726bfc04e248a4d6738a1292e935f68c3449786b
nullswan <pro@nullswan.io>
2024-04-09 11:29:46 +0200

    feat: recursively walk subdirectories

--- src/sembump/bumper/walker.py
+++ src/sembump/bumper/walker.py
@@ -4,8 +4,15 @@ from typing import Generator
 TARGET_FILE = "pyproject.toml"
 
 
-def find_pyproject_files(root_directory: str) -> Generator[str, None, None]:
-    for root, _, files in walk(root_directory):
+def find_pyproject_files(
+    root_directory: str,
+) -> Generator[str, None, None]:
+    for root, directory, files in walk(root_directory):
+        for subdirectory in directory:
+            if subdirectory.startswith("."):
+                continue
+            yield from find_pyproject_files(path.join(root, subdirectory))
+
         if TARGET_FILE not in files:
             continue
 
